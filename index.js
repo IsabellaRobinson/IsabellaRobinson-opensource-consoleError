87194940af93ba94e6485aa7ef435a3d1ea49094
Isabella Robinson <134569@rts-365.dk>
2020-10-22 12:45:16 +0200

    demo - Warning & Error

---
+++ index.js
@@ -0,0 +1,10 @@
+var reset = "\x1b[0m"
+
+var colorConsole = {
+    error: function(string) {
+        console.error("\x1b[31m" + "ERROR:\t" + string + reset);
+    },
+    warning: function(string) {
+        console.warn("\x1b[30m" + "WARNING:\t" + string + reset);
+    }
+}
