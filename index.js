60e53bf607f8fde29ae78c2e0b1aa310be381164
Isabella Robinson <134569@rts-365.dk>
2020-10-22 13:43:45 +0200

    more add to index.js and index.html

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ var colorConsole = {
     },
     warning: function(string) {
         console.warn("\x1b[30m" + "WARNING:\t" + string + reset);
+    },
+    success: function(string){
+        console.info("\x1b[32m"  + "SUCCESS:\t" + string + reset);
+    },
+    magenta: function(string){
+        console.log("\x1b[35m" + "MAGENTA:\t" + string + reset)
     }
 }
